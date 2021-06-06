2fef1e5a5dde0963f0cddd3f207c23eeafe9bb15
Mykel-Eze <michaeleze1997@gmail.com>
2021-06-06 23:06:59 +0100

    Banner done

---
+++ src/components/TopBannerForm.vue
@@ -0,0 +1,50 @@
+<template>
+    <form action="#" class="col s12">
+        <div class="col s12">
+            <h5 class="form-title sec-color">Free speeding ticket review</h5>
+        </div>
+        <div class="col s12 pad0">
+            <div class="input-field col s12 m6">
+                <input id="first_name" name="first_name" type="text" class="">
+                <label for="first_name">First Name</label>
+                <img src="../assets/images/man.svg" alt="icon" class="input-icons">
+            </div>
+            <div class="input-field col s12 m6">
+                <input id="last_name" name="last_name" type="text" class="">
+                <label for="last_name">Last Name</label>
+                <img src="../assets/images/man.svg" alt="icon" class="input-icons">
+            </div>
+            <div class="input-field col s12 m12">
+                <input id="phone_number" name="phone_number" type="tel" class="">
+                <label for="phone_number">Phone Number</label>
+                <img src="../assets/images/smartphone.svg" alt="icon" class="input-icons">
+            </div>
+            <div class="input-field col s12 m12">
+                <input id="email" name="email" type="email" class="">
+                <label for="email">Email Address</label>
+                <img src="../assets/images/email.svg" alt="icon" class="input-icons">
+            </div>
+        </div>
+        <div class="col s12">
+            <div class="flex-div justify-content-btw form-links-cont">
+                <div class="get-started-home">
+                    Fight My Ticket? 
+                    <router-link to="#" class="sec-color"><span>Get Started</span></router-link>
+                </div>
+                <div>
+                    <button type="submit" class="submit-btn pry-btn btn">Submit</button>
+                </div>
+            </div>
+        </div>
+    </form>
+</template>
+
+<script>
+export default {
+    name: "TopBannerForm"
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/SpeedingTicket.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <section id="home">
+        <div class="row">
+            <div class="container">
+                <div class="col s12 m7" id="title-side">
+                    <h1 class="home-title">
+                        Beat your California Speeding Ticket fast and effectively.
+                    </h1>
+                    <div class="home-btn-wrapper">
+                        <button class="pry-btn btn">
+                            Can My Case Be Beat? <img src="../assets/images/calculator.svg" alt="icon">
+                        </button>
+                        <button class="pry-btn btn">
+                            Snipe My Ticket <img src="../assets/images/target-icon.svg" alt="icon">
+                        </button>
+                    </div>
+                    <div class="home-learn-more">
+                        <a href="#">
+                            <img src="../assets/images/play-white.svg" alt="play">
+                            <span>Learn More with a Free Ticket Review</span>
+                        </a>
+                    </div>
+                </div>
+                <div class="col s12 m5 pad0" id="form-side">
+                    <TopBannerForm />
+                </div>
+            </div>
+        </div>
+    </section>
+  </div>
+</template>
+
+<script>
+import TopBannerForm from '../components/TopBannerForm.vue'
+export default {
+    name: "SpeedingTicket",
+    components: { 
+        TopBannerForm 
+    }
+}
+</script>
+
+<style scoped>
+#home {
+    background-image: url(../assets/images/speeding-ticket-bg.jpg);
+}
+</style>
