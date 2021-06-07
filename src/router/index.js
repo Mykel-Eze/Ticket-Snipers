a8002e8147746758cb394e100c06a0bc9c2b6181
Mykel-Eze <michaeleze1997@gmail.com>
2021-06-07 03:51:24 +0100

    Other ticket pages done

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import SpeedingTicket from '../views/SpeedingTicket.vue'
+import RedLightCamera from '../views/RedLightCamera.vue'
+import GeneralTraffic from '../views/GeneralTraffic.vue'
+import StopSignTicket from '../views/StopSignTicket.vue'
+import RedLightTicket from '../views/RedLightTicket.vue'
 
 const routes = [
   {
@@ -12,6 +16,26 @@ const routes = [
     path: '/speeding-ticket',
     name: 'SpeedingTicket',
     component: SpeedingTicket
+  },
+  {
+    path: '/red-light-camera',
+    name: 'RedLightCamera',
+    component: RedLightCamera
+  },
+  {
+    path: '/general-traffic',
+    name: 'GeneralTraffic',
+    component: GeneralTraffic
+  },
+  {
+    path: '/stop-sign-ticket',
+    name: 'StopSignTicket',
+    component: StopSignTicket
+  },
+  {
+    path: '/red-light-ticket',
+    name: 'RedLightTicket',
+    component: RedLightTicket
   }
 ]
 
