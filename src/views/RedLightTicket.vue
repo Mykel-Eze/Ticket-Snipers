1b20000dee8f238beeea567d18efe7d2556f5223
Mykel-Eze <michaeleze1997@gmail.com>
2021-06-09 02:52:42 +0100

    Calculator Trigger Section made into a component

--- src/views/RedLightTicket.vue
+++ src/views/RedLightTicket.vue
@@ -92,33 +92,18 @@
         </div>
     </section>
 
-    <section id="calculator-sec">
-        <div class="row">
-            <div class="container">
-                <div class="calc-sec-wrapper">
-                    <h2 class="calc-sec-title">Ticket Dismissal Calculator</h2>
-                    <div class="home-learn-more">
-                        <a href="#">
-                            <img src="../assets/images/play-white.svg" alt="play">
-                            <span>Learn More with a Free Ticket Review</span>
-                        </a>
-                    </div>
-                    <button class="pry-btn btn white-bg-btn">
-                        Can My Case Be Beat? <img src="../assets/images/calculator-blue.svg" alt="icon">
-                    </button>
-                </div>
-            </div>
-        </div>
-    </section>
+    <CalculatorTriggerSect />
   </div>
 </template>
 
 <script>
 import TopBannerForm from '../components/TopBannerForm.vue'
+import CalculatorTriggerSect from '../components/CalculatorTriggerSect.vue'
 export default {
     name: "RedLightTicket",
     components: { 
-        TopBannerForm 
+        TopBannerForm,
+        CalculatorTriggerSect 
     }
 }
 </script>
